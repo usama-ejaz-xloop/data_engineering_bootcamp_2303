c5c966d2daf89830bc974596bde7b311b09606bc
Usama Ejaz <119282773+usama-ejaz-xloop@users.noreply.github.com>
2023-04-19 11:13:33 +0500

    Added 3.1, 3.2

---
+++ tasks/3_machine_learning_essentials/day_1_machine_learning_basics/assignment.py
@@ -0,0 +1,12 @@
+import pandas as pd # type: ignore
+from typing import List
+
+
+def label_encoder(df: pd.DataFrame, columns_to_encode: List[str]) -> pd.DataFrame:
+
+    # Implement a function that modifies only columns of the `df` DataFrame specified in
+    # `columns_to_encode` argument. You should follow label encoding approach
+    # described in the lecture.
+
+    pass
+
